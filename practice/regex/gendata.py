011c53dfd13d0dea17f68e283838274d82dc6269
wangyuchen <ladesiji@gmail.com>
2019-03-28 14:59:31 +0800

    python 核心编程3 练习

---
+++ practice/regex/gendata.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+    生成随机 登陆 日志
+    包括 日期 - 邮箱地址 
+"""
+
+from random import randrange, choice
+from string import ascii_lowercase as lc
+from time import ctime
+
+tlds = ('com', 'edu', 'net', 'org', 'gov')
+
+for i in range(randrange(5, 11)):
+    dtint = randrange(2**32)      # 选取随机数
+    dtstr = ctime(dtint)          # 选取日期
+    llen = randrange(4, 8)        # login 长度
+    login = ''.join(choice(lc) for j in range(llen))
+    dlen = randrange(llen, 13)    # 域名 长度
+    dom = ''.join(choice(lc) for j in range(dlen))
+    print(f'{dtstr}::{login}@{dom}.{choice(tlds)}::{dtint}-{llen}-{dlen}')
+
