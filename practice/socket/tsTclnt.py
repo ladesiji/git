6fa3525f5463c9ddf6f1cfa1aae8cecdc1789c4b
wangyuchen <ladesiji@gmail.com>
2019-03-29 16:26:05 +0800

    add tsT server / client

---
+++ practice/socket/tsTclnt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+"""
+    创建TCP客户端
+    提示用户输入，并发关到服务器
+    接收服务器返回的添加了时间戳的相同消息
+    并显示结果
+"""
+
+import socket
+
+HOST = '192.168.42.1'
+PORT = 8421
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while True:
+    data = input('> ')
+    if not data:
+        break
+    tcpCliSock.send(data.encode('utf-8'))
+    data = tcpCliSock.resv(BUFSIZ)
+    if not data:
+        break
+    print(data.decode())
+
+tcpCliSock.close()
---
+++ practice/socket/tsTserv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import socket
+from time import ctime
+
+HOST = ""
+PORT = 8421
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+while True:
+    print("waiting for connection...")
+    tcpCliSock, addr = tcpSerSock.accept()
+    print(f"...connected from:{addr}")
+
+    while True:
+        data = tcpCliSock.recv(BUFSIZ).decode()
+        if not data:
+            break
+        tcpCliSock.send(f'{ctime()} {data}'.encode('utf-8'))
+
+    tcpCliSock.close()
+tcpSerSock.close()
