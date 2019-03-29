dbb78c9bf07a1b2ca65db69afeec5570392f39fa
wangyuchen <ladesiji@gmail.com>
2019-03-29 11:54:54 +0800

    完成第一章习题

--- practice/regex/gendata.py
+++ practice/regex/gendata.py
@@ -9,7 +9,7 @@ from string import ascii_lowercase as lc
 from time import ctime
 
 tlds = ('com', 'edu', 'net', 'org', 'gov')
-
+txt = ""
 for i in range(randrange(5, 11)):
     dtint = randrange(2**32)      # 选取随机数
     dtstr = ctime(dtint)          # 选取日期
@@ -17,5 +17,6 @@ for i in range(randrange(5, 11)):
     login = ''.join(choice(lc) for j in range(llen))
     dlen = randrange(llen, 13)    # 域名 长度
     dom = ''.join(choice(lc) for j in range(dlen))
-    print(f'{dtstr}::{login}@{dom}.{choice(tlds)}::{dtint}-{llen}-{dlen}')
-
+    txt += f'{dtstr}::{login}@{dom}.{choice(tlds)}::{dtint}-{llen}-{dlen}'+"\n"
+with open('redata.txt', 'w') as f:
+    f.write(txt)
