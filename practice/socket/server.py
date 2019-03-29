7516481d4fec960f1b6543a0e544ce3180249d41
wangyuchen <ladesiji@gmail.com>
2019-03-29 15:02:44 +0800

    all bugs is clear

--- practice/socket/server.py
+++ practice/socket/server.py
@@ -18,7 +18,7 @@ ss.listen(5)            # 等待客户端连接
 while True:
     cs = ss.accept()    # 建立客户端连接
     print(f"{cs[1]} 连接成功")
-    print(cs[0].resv(1024).decode())
+    print('receve:'cs[0].recv(1024).decode())
     cs[0].send(f"欢迎{cs[1]}访问 {host}".encode('utf-8'))
     cs[0].close         # 关闭连接
 
